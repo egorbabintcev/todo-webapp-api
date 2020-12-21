e5a02017139e837534ebb7bdd17a736a6dec2dc3
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-12-21 18:37:56 +0300

    Lint code

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,17 +3,15 @@ import { IsNotEmpty, Matches, MinLength } from 'class-validator';
 export class CreateUserDto {
   @IsNotEmpty()
   @MinLength(2)
-  @Matches(
-    /^[a-z0-9_-]+$/igm,
-    { message: 'Login must contain letter, digits, _ and - characters' }
-  )
+  @Matches(/^[a-z0-9_-]+$/gim, {
+    message: 'Login must contain letter, digits, _ and - characters',
+  })
   username: string;
 
   @IsNotEmpty()
   @MinLength(6)
-  @Matches(
-    /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-zA-Z])\S*$/gm,
-    { message: 'Password must contain letters, digits and special characters' },
-  )
+  @Matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-zA-Z])\S*$/gm, {
+    message: 'Password must contain letters, digits and special characters',
+  })
   password: string;
 }
